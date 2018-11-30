ea7c0292b0325fccfa34d4962095d820617b041f
Nathan Dines <nath@ndin.es>
2018-11-30 14:54:24 +1100

    forge: v2.2.5

--- forge.rb
+++ forge.rb
@@ -1,21 +1,19 @@
 class Forge < Formula
   desc "Continuous Delivery friendly CloudFormation deployment tool"
   homepage "https://github.com/nathandines/forge"
-  url "https://github.com/nathandines/forge/archive/v2.2.5.tar.gz"
-  sha256 "a3d909c9f004eec2c7d20df7730a84781425969461141efe9f14f72c2f85bb9d"
+  url "https://github.com/nathandines/forge/releases/download/v2.2.5/forge_v2.2.5_darwin_amd64"
+  version "2.2.5"
+  sha256 "88c178bafc330b573547dceaf1d8a9e8c17a7e56eaf2fdef4cbfdce21ced6e00"
 
   bottle :unneeded
 
-  depends_on "go" => :build
-
   def install
-    system "make", "build"
-
     bin.mkpath
-    bin.install "bin/forge"
+    bin.install "forge_v2.2.5_darwin_amd64"
+    mv bin/"forge_v2.2.5_darwin_amd64", bin/"forge"
 
-    (bash_completion/"forge").write(`#{bin}/forge gen-bash-completion`)
-    (zsh_completion/"forge").write(`#{bin}/forge gen-zsh-completion`)
+    (bash_completion/"forge").write(bin/"forge gen-bash-completion")
+    (zsh_completion/"forge").write(bin/"forge gen-zsh-completion")
 
     prefix.install_metafiles
   end
