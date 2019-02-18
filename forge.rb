18fd1215cb807f5a3932a18f9f1a10bfcb73087c
Nathan Dines <nath@ndin.es>
2019-02-18 07:19:11 +0000

    forge: v2.3.0

--- forge.rb
+++ forge.rb
@@ -1,16 +1,16 @@
 class Forge < Formula
   desc "Continuous Delivery friendly CloudFormation deployment tool"
   homepage "https://github.com/nathandines/forge"
-  url "https://github.com/nathandines/forge/releases/download/v2.2.5/forge_v2.2.5_darwin_amd64"
-  version "2.2.5"
-  sha256 "88c178bafc330b573547dceaf1d8a9e8c17a7e56eaf2fdef4cbfdce21ced6e00"
+  url "https://github.com/nathandines/forge/releases/download/v2.3.0/forge_v2.3.0_darwin_amd64"
+  version "2.3.0"
+  sha256 "2cfdccf06148664f15a8d9b72175cb0cf7bd522fcf71c7516a2da49329b43029"
 
   bottle :unneeded
 
   def install
     bin.mkpath
-    bin.install "forge_v2.2.5_darwin_amd64"
-    mv bin/"forge_v2.2.5_darwin_amd64", bin/"forge"
+    bin.install "forge_v2.3.0_darwin_amd64"
+    mv bin/"forge_v2.3.0_darwin_amd64", bin/"forge"
     chmod 0755, bin/"forge"
 
     (bash_completion/"forge").write(`#{bin}/forge gen-bash-completion`)
